996ae4ff49f68c611db49f987e3c5bcebb4c8d34
Willing <CuiHongwei1999@outlook.com>
2025-02-28 15:54:30 +0800

    Randomly select a background image on loading the webpage.

--- assets/js/dynamic_background.js
+++ assets/js/dynamic_background.js
@@ -1,17 +1,41 @@
 const images = [
-    'bg1.jpg',
-    'bg2.jpg',
-    'bg3.jpg'
-  ];
-  
-  const bgElement = document.getElementById('bg'); // Grab the <div> element
-  
-  // Function to set a random background image
-  function setRandomBackground() {
-    const randomImage = images[Math.floor(Math.random() * images.length)]; // Randomly select an image
-    bgElement.style.backgroundImage = `url('${randomImage}')`; // Change the background image
-  }
-  
-  // Set the random background when the page loads
-  window.onload = setRandomBackground;
-  
+  './images/bg1.jpg',
+  './images/bg2.jpg',
+  './images/bg3.jpg',
+  './images/bg4.jpg',
+  './images/bg5.jpg',
+  './images/bg6.jpg',
+  './images/bg7.jpg',
+  './images/bg8.jpg',
+  './images/bg9.jpg',
+  './images/bg10.jpg',
+  './images/bg11.jpg',
+  './images/bg12.jpg',
+  './images/bg13.jpg',
+  './images/bg14.jpg',
+  './images/bg15.jpg',
+  './images/bg16.jpg',
+  './images/bg17.jpg',
+  './images/bg18.jpg',
+  './images/bg19.jpg',
+  './images/bg20.jpg'
+];
+
+// Function to set a random background image
+function setRandomBackground() {
+  const randomImage = images[Math.floor(Math.random() * images.length)]; // Randomly select an image
+
+  // Create a <style> element to update the :after pseudo-element
+  const style = document.createElement('style');
+  style.innerHTML = `
+    #bg:after {
+      background-image: url('${randomImage}');
+    }
+  `;
+
+  // Add the new style to the document
+  document.head.appendChild(style);
+}
+
+// Set the random background when the page loads
+window.onload = setRandomBackground;
