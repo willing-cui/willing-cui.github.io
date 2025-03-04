ede3f54e1cdf1001d837b69cd553018da3298409
Willing <CuiHongwei1999@outlook.com>
2025-03-04 12:18:24 +0800

    Fix bugs in typewriter.js
    The typing animation will be played on loading WORK page and on clicking the WORK button.

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -1,21 +1,29 @@
 var str = 'Do something great!';
 var i = 0;
+var timer = undefined;
 
 function typing() {
     var typer = document.getElementById('typewriter');
     if (i <= str.length) {
         typer.innerHTML = str.slice(0, i++) + '_';
-        setTimeout('typing()', 200);    // 递归调用
+        timer = setTimeout('typing()', 250);    // 递归调用
     }
     else {
         if (i % 2 == 0) {
-            typer.innerHTML = str;  // 结束打字, 移除 _ 光标
+            typer.innerHTML = str;              // 结束打字, 移除 _ 光标
         } else {
             typer.innerHTML = str + '_';
         }
         i++;
-        setTimeout('typing()', 500);    // 递归调用
+        timer = setTimeout('typing()', 500);    // 递归调用
     }
 }
 
-typing();
+function startTyping() {
+    clearTimeout(timer);
+    i = 0; typing();
+}
+
+const workButton = document.getElementById("workButton");
+workButton.addEventListener("click", () => { startTyping(); });
+window.addEventListener("load", () => { startTyping(); });
